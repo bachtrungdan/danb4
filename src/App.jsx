ce314344f05d655b2fd76a2e421a3275f7520788
bachdan <bachtrungdan@gmail.com>
2022-11-07 22:35:40 +0700

    capnhat

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,13 @@ function App() {
 
   return (
     <div>
-      <button type="button" className="btn btn-primary " id='dan' data-bs-toggle="modal" data-bs-target="#exampleModal"onClick={clear}>
+
+      If you're familiar with the JavaScript ecosystem, React, and React Router, this serves as a quick overview of React Router v6 with lots of code and minimal explanations.
+
+      For a complete introduction to React Router, do the Tutorial
+      For extensive documentation on every API, see API Reference
+      For a deeper understanding of concepts, see Main Concepts
+      <button type="button" className="btn btn-primary " id='dan' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={clear}>
         <h3> todoList</h3>
       </button>
       <ListModal listData={listData} setIndex={setIndex} />
